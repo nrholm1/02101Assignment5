8f65786683d17a32c07e768907fbca48840ac396
Niels Raunkjær <nielsraunkjaer@gmail.com>
2020-11-24 11:18:23 +0100

    Refactored TestDriver into Driver class with a better interface and TestDriver with main method for running different configurations

---
+++ src/Opgave3/Driver.java
@@ -0,0 +1,58 @@
+package Opgave3;
+
+import java.io.FileNotFoundException;
+
+public class Driver {
+    static Mandelbrot mb = new Mandelbrot();
+    static ColorPicker colorProfile;
+
+    static void drawWrapper() {
+        StdDraw.show(1);
+        drawFractal(colorProfile);
+        StdDraw.show();
+    }
+
+    static void drawFractal(ColorPicker colors) {
+        Complex z = null;
+        for(int i = 0; i < mb.getGridSize(); i++) {
+            for(int j = 0; j < mb.getGridSize(); j++) {
+                z = mb.calculateGridCoords(i, j);
+                z.setIterationValue(mb.iterate(z));
+
+                StdDraw.setPenColor(colors.map.get(z.getIterationValue()));
+
+                StdDraw.point(z.getRe(), z.getIm());
+            }
+        }
+    }
+
+    static void configure() {
+        double[] doubles = mb.promptDoubles("Enter 3 numbers: ");
+        mb.setViewParams(doubles);
+
+        colorProfile = new ColorPicker();
+
+        setStdDrawSettings(mb.getCenterX(),
+                mb.getCenterY(),
+                mb.getSideLength());
+    }
+
+    static void configure(String fileName) throws FileNotFoundException {
+        double[] doubles = {0.10684, -0.63675, 0.0085};
+        mb.setViewParams(doubles);
+
+        int[][] colorGrid = ColorParser.parseFile(fileName);
+        colorProfile = new ColorPicker(colorGrid);
+
+        setStdDrawSettings(mb.getCenterX(),
+                mb.getCenterY(),
+                mb.getSideLength());
+    }
+
+
+    static void setStdDrawSettings(double _x, double _y, double sideLength) {
+        StdDraw.setXscale(_x - sideLength / 2, _x + sideLength / 2);
+        StdDraw.setYscale(_y - sideLength / 2, _y + sideLength / 2);
+        StdDraw.setPenRadius(1.0/450);
+    }
+}
--- src/Opgave3/TestDriver.java
+++ src/Opgave3/TestDriver.java
@@ -6,43 +6,8 @@ import java.io.FileNotFoundException;
 public class TestDriver {
     static Mandelbrot mb = new Mandelbrot();
     public static void main(String args[]) throws FileNotFoundException {
-//        double[] doubles = mb.promptDoubles("Enter 3 numbers: ");
-        double[] doubles = {0.10684, -0.63675, 0.0085};
-        mb.setViewParams(doubles);
-
-        ColorPicker randomColorProfile = new ColorPicker();
-
-        int[][] colorGrid = ColorParser.parseFile("blues");
-
-        ColorPicker colorProfile = new ColorPicker(colorGrid);
-        setStdDrawSettings(mb.getCenterX(),
-                            mb.getCenterY(),
-                            mb.getSideLength());
-
-        StdDraw.show(1);
-//        drawFractal(randomColorProfile);
-        drawFractal(colorProfile);
-        StdDraw.show();
-    }
-
-    static void drawFractal(ColorPicker colors) {
-        Complex z = null;
-        for(int i = 0; i < mb.getGridSize(); i++) {
-            for(int j = 0; j < mb.getGridSize(); j++) {
-                z = mb.calculateGridCoords(i, j);
-                z.setIterationValue(mb.iterate(z));
-
-                StdDraw.setPenColor(colors.map.get(z.getIterationValue()));
-
-                StdDraw.point(z.getRe(), z.getIm());
-            }
-        }
-    }
-
-    static void setStdDrawSettings(double _x, double _y, double sideLength) {
-        StdDraw.setXscale(_x - sideLength / 2, _x + sideLength / 2);
-        StdDraw.setYscale(_y - sideLength / 2, _y + sideLength / 2);
-        StdDraw.setPenRadius(1.0/450);
+        Driver.configure("mandel");
+        Driver.drawWrapper();
     }
 }
 
